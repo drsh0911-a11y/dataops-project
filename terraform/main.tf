47649140792a1fc909cd5057ff05b9e3f13d6ea2
Mustapha Emad <drsh0911@gmail.com>
2026-09-22 16:21:14 +0300

    Add Terraform configuration for Docker provider

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+﻿resource "docker_image" "pipeline" {
+  name         = var.image_name
+  keep_locally = true
+  build {
+    context = ".."
+  }
+}
+
+resource "docker_container" "pipeline" {
+  image = docker_image.pipeline.image_id
+  name  = var.container_name
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+﻿variable "image_name" {
+  type    = string
+  default = "dataops-pipeline"
+}
+
+variable "container_name" {
+  type    = string
+  default = "dataops-pipeline-container"
+}
